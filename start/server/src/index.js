a45606557781edbe9071171fee54d36bf064b289
Ivan Papko <ivanpapko@gmail.com>
2021-11-06 23:59:28 +0300

    3. Write query resolvers

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
-
+const resolvers = require('./resolvers');
 
 const { createStore } = require('./utils');
 
@@ -14,6 +14,7 @@ const store = createStore();
 
 const server = new ApolloServer({
     typeDefs,
+    resolvers,
     dataSources: () => ({
         launchAPI: new LaunchAPI(),
         userAPI: new UserAPI({ store })
