07eac7780a5cdde590eb0058661d8a15019805e6
Ivan Papko <ivanpapko@gmail.com>
2021-11-07 17:40:37 +0300

    6. Set up Apollo Client

---
+++ start/client/src/index.tsx
@@ -0,0 +1,11 @@
+import {
+    ApolloClient,
+    gql,
+    NormalizedCacheObject
+} from '@apollo/client';
+import { cache } from './cache';
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+    cache,
+    uri: 'http://localhost:4000/graphql'
+});
