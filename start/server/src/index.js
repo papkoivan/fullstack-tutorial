b47f696fab11deae462c8e1ed670bdfbcf11100a
Ivan Papko <ivanpapko@gmail.com>
2021-11-07 12:40:17 +0300

    4. Write mutation resolvers

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const isEmail = require('isemail');
 
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
@@ -13,12 +14,22 @@ const store = createStore();
 
 
 const server = new ApolloServer({
+    context: async ({ req }) => {
+        // simple auth check on every request
+        const auth = req.headers && req.headers.authorization || '';
+        const email = Buffer.from(auth, 'base64').toString('ascii');
+        if (!isEmail.validate(email)) return { user: null };
+        // find a user by their email
+        const users = await store.users.findOrCreate({ where: { email } });
+        const user = users && users[0] || null;
+        return { user: { ...user.dataValues } };
+    },
     typeDefs,
     resolvers,
     dataSources: () => ({
         launchAPI: new LaunchAPI(),
         userAPI: new UserAPI({ store })
-    })
+    }),
 });
 
 server.listen().then(() => {
