c0084934a2d71032129eeca2228563f1556ec348
Ivan Papko <IvanPapko@coherentSolutions.com>
2021-11-08 12:41:48 +0300

    Add pagination support

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,7 +1,7 @@
-import React, { Fragment, useState }  from 'react';
-import { RouteComponentProps } from '@reach/router';
-import { gql, useQuery } from '@apollo/client';
-import { LaunchTile, Header, Button, Loading } from '../components';
+import React, {Fragment, useState} from 'react';
+import {RouteComponentProps} from '@reach/router';
+import {gql, useQuery} from '@apollo/client';
+import {Header, LaunchTile, Button, Loading} from '../components';
 import * as GetLaunchListTypes from './__generated__/GetLaunchList';
 
 export const LAUNCH_TILE_DATA = gql`
@@ -37,29 +37,46 @@ export const GET_LAUNCHES = gql`
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
-  const {
-    data,
-    loading,
-    error
-  } = useQuery<
-      GetLaunchListTypes.GetLaunchList,
-      GetLaunchListTypes.GetLaunchListVariables
-      >(GET_LAUNCHES);
+    const {
+        data,
+        loading,
+        error,
+        fetchMore,
+    } = useQuery<GetLaunchListTypes.GetLaunchList,
+        GetLaunchListTypes.GetLaunchListVariables>(GET_LAUNCHES);
 
-  if (loading) return <Loading />;
-  if (error) return <p>ERROR</p>;
-  if (!data) return <p>Not found</p>;
+    const [isLoadingMore, setIsLoadingMore] = useState(false);
+    if (loading) return <Loading />;
+    if (error) return <p>ERROR</p>;
+    if (!data) return <p>Not found</p>;
 
-  return (
-      <Fragment>
-        <Header />
-        {data.launches &&
-        data.launches.launches &&
-        data.launches.launches.map((launch: any) => (
-            <LaunchTile key={launch.id} launch={launch} />
-        ))}
-      </Fragment>
-  );
+    return (
+        <Fragment>
+            <Header />
+            {data.launches &&
+            data.launches.launches &&
+            data.launches.launches.map((launch: any) => (
+                <LaunchTile key={launch.id} launch={launch} />
+            ))}
+            {data.launches && data.launches.hasMore && (
+              isLoadingMore
+                  ? <Loading />
+                  : <Button
+                      onClick={async () => {
+                          setIsLoadingMore(true);
+                          await fetchMore({
+                              variables: {
+                                  after: data.launches.cursor,
+                              },
+                          });
+                          setIsLoadingMore(false);
+                      }}
+                  >
+                      Load More
+                  </Button>
+            )}
+        </Fragment>
+    );
 }
 
 
