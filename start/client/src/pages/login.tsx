a6f0a0eb21ae41b6f9262e62066b0bce441addc6
Ivan Papko <ivanpapko@gmail.com>
2021-11-10 22:35:01 +0300

    9. Manage local state

--- start/client/src/pages/login.tsx
+++ start/client/src/pages/login.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { gql, useMutation } from '@apollo/client';
+import {gql, useMutation} from '@apollo/client';
 
-import { LoginForm, Loading } from '../components';
+import {LoginForm, Loading} from '../components';
 import * as LoginTypes from './__generated__/Login';
+import {isLoggedInVar} from '../cache';
 
 export const LOGIN_USER = gql`
   mutation Login($email: String!) {
@@ -14,20 +15,21 @@ export const LOGIN_USER = gql`
 `;
 
 export default function Login() {
-  const [login, { loading, error }] = useMutation<LoginTypes.Login, LoginTypes.LoginVariables>(
-          LOGIN_USER,
-      {
-                  onCompleted({login}) {
-                    if (login) {
-                      localStorage.setItem('token', login.token as string);
-                      localStorage.setItem('userId', login.id as string);
-                    }
-                  }
-              }
-  );
+    const [login, {loading, error}] = useMutation<LoginTypes.Login, LoginTypes.LoginVariables>(
+        LOGIN_USER,
+        {
+            onCompleted({login}) {
+                if (login) {
+                    localStorage.setItem('token', login.token as string);
+                    localStorage.setItem('userId', login.id as string);
+                    isLoggedInVar(true);
+                }
+            }
+        }
+    );
 
-  if (loading) return <Loading />;
-  if (error) return <p>An error occurred</p>;
+    if (loading) return <Loading/>;
+    if (error) return <p>An error occurred</p>;
 
-  return <LoginForm login={login} />;
+    return <LoginForm login={login}/>;
 }
